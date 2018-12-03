ebba3b839d02433384ac354a5a75b84da9d9b755
Konstantinos Vandikas <konstantinos.vandikas@ericsson.com>
2018-12-03 12:33:06 +0100

    include proper notebook

--- docker/start_jupyter.sh
+++ docker/start_jupyter.sh
@@ -2,4 +2,4 @@
 
 pip install jupyter ipython==5.0.0 ipykernel==4.8.0
 
-jupyter notebook --allow-root --port 9090 --NotebookApp.token='' --ip=10.68.229.7 --no-browser
+jupyter notebook --allow-root --port 9090 --NotebookApp.token='' --ip=0.0.0.0 --no-browser
